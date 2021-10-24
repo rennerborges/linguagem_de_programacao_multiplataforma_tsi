bdea0767051a797ebb06a70b25da852bdca850de
Renner Borges <rennerferreira23@gmail.com>
2021-10-24 15:42:40 -0300

    feat: create Fornecedor Class

--- T1.1/src/t1/pkg1/Main.java
+++ T1.1/src/t1/pkg1/Main.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import t1.pkg1.controller.Endereco;
 import t1.pkg1.controller.Telefone;
 import t1.pkg1.controller.Cliente;
+import t1.pkg1.controller.Fornecedor;
 
 /**
  *
@@ -21,19 +22,32 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Endereco endereco = new Endereco("Rua x", "Morrinhos");
-        Telefone telefone1 = new Telefone("64", "99999999");
-        Telefone telefone2 = new Telefone("44", "00000000");
+        Endereco enderecoCliente = new Endereco("Rua x", "Morrinhos");
+        Telefone telefoneCliente1 = new Telefone("64", "99999999");
+        Telefone telefoneCliente2 = new Telefone("44", "00000000");
 
         
-        ArrayList<Telefone> telefones = new ArrayList();
+        ArrayList<Telefone> telefonesCliente = new ArrayList();
         
-        telefones.add(telefone1);
-        telefones.add(telefone2);
+        telefonesCliente.add(telefoneCliente1);
+        telefonesCliente.add(telefoneCliente2);
         
-        Cliente renner = new Cliente("Renner", "0000000000",telefones, endereco);
+        Cliente renner = new Cliente("Renner", "0000000000",telefonesCliente, enderecoCliente);
         
         renner.print();
+        
+        Endereco enderecoFornecedor = new Endereco("Rua y", "Caldas Novas");
+        Telefone telefoneFornecedor1 = new Telefone("84", "84444444");
+        Telefone telefoneFornecedor2 = new Telefone("11", "87444444");
+
+        ArrayList<Telefone> telefonesFornecedor = new ArrayList();
+        
+        telefonesFornecedor.add(telefoneFornecedor1);
+        telefonesFornecedor.add(telefoneFornecedor2);
+        
+        Fornecedor gisele = new Fornecedor("Gisele", "000000", telefonesFornecedor, enderecoFornecedor);
+        
+        gisele.print();
           
 
         
---
+++ T1.1/src/t1/pkg1/controller/Fornecedor.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t1.pkg1.controller;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ *
+ * @author renner
+ */
+public class Fornecedor {
+       private String nome;
+       private String cnpj;
+       private ArrayList<Telefone> telefones;
+       private Endereco endereco;
+
+    public Fornecedor(String nome, String cnpj, ArrayList<Telefone> telefones, Endereco endereco) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.telefones = telefones;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public ArrayList<Telefone> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(ArrayList<Telefone> telefones) {
+        this.telefones = telefones;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Fornecedor other = (Fornecedor) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.cnpj, other.cnpj)) {
+            return false;
+        }
+        if (!Objects.equals(this.telefones, other.telefones)) {
+            return false;
+        }
+        if (!Objects.equals(this.endereco, other.endereco)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Fornecedor{" + "nome=" + nome + ", cnpj=" + cnpj + ", telefones=" + telefones + ", endereco=" + endereco + '}';
+    }
+       
+    public void print(){
+        System.out.println(this);
+    }
+}
