ce990bbbb19ca3fc9f7519fbce4f5ec577632b2f
Renner Borges <rennerferreira23@gmail.com>
2021-12-05 19:34:43 -0300

    começando a separar funcionalidades

--- Calculadora/src/main/java/com/mycompany/calculadora/PrimaryController.java
+++ Calculadora/src/main/java/com/mycompany/calculadora/PrimaryController.java
@@ -3,16 +3,27 @@ package com.mycompany.calculadora;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class PrimaryController {
-
+    
+    @FXML
+    private TextField visor;
+    
+    @FXML
+    void saveNumber(MouseEvent event) {
+       Button button = ((Button) event.getSource());
+       int value = Integer.parseInt(button.getText());
+       visor.setText(button.getText());
+    }
+    
     @FXML
     void saveOperation(MouseEvent event) {
        Button button = ((Button) event.getSource());
-       int value = Integer.parseInt(button.getText());
-       System.out.println(value);
+       visor.setText(button.getText());
     }
+    
     
     @FXML
     private void switchToSecondary() throws IOException {
